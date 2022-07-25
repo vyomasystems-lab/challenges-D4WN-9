29b3a549b5b5e93dc54c8e09f4261ef8a6756b48
D4WN-9 <nononishant@gmail.com>
2022-07-25 17:08:46 +0000

    dood

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -1,10 +1,22 @@
-# See LICENSE.vyoma for details
+# # See LICENSE.vyoma for details
 
-import cocotb
-from cocotb.triggers import Timer
+# import cocotb
+# from cocotb.triggers import Timer
+import numpy as np
+# @cocotb.test()
+# async def test_mux(dut):
+#     """Test for mux2"""
 
-@cocotb.test()
-async def test_mux(dut):
-    """Test for mux2"""
+input_arr = np.zeros(31)
+input_arr[0]=1
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+for i in range(31):
+    x = np.roll(input_arr, i)
+    for j in range(31):
+        temp = 'dut.inp' + str(j) + '.value = ' + str(int(x[j]))
+        print(temp)
+        for k in range(31):
+            dut.sel.value = k
+            assert dut.out.value == x[k]
+
+    # cocotb.log.info('##### CTB: Develop your test here ########')
