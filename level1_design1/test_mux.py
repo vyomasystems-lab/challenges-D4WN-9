dee8487ccc403b13255821792a4ab69cd5810871
D4WN-9 <nononishant@gmail.com>
2022-07-25 18:11:26 +0000

    update

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -1,22 +1,19 @@
-# # See LICENSE.vyoma for details
-
-# import cocotb
-# from cocotb.triggers import Timer
+import cocotb
+from cocotb.triggers import Timer
 import numpy as np
-# @cocotb.test()
-# async def test_mux(dut):
-#     """Test for mux2"""
 
-input_arr = np.zeros(31)
-input_arr[0]=1
+@cocotb.test()
+async def mux_exhaustive_test(dut):
+	input_arr = np.zeros(31)
+	input_arr[0]=1
 
-for i in range(31):
-    x = np.roll(input_arr, i)
-    for j in range(31):
-        temp = 'dut.inp' + str(j) + '.value = ' + str(int(x[j]))
-        print(temp)
-        for k in range(31):
-            dut.sel.value = k
-            assert dut.out.value == x[k]
-
-    # cocotb.log.info('##### CTB: Develop your test here ########')
+	for i in range(31):
+		x = np.roll(input_arr, i)
+		for j in range(31):
+			temp = 'dut.inp' + str(j) + '.value = ' + str(int(x[j]))
+			exec(temp)
+			for k in range(31):
+				dut.sel.value = k
+				await Timer(1, units='ns')
+				cocotb.log.info('##### CTB: Develop your test here ########')
+				assert dut.out.value == x[k], 'TEST FAIL with sel={k}'.format(k=dut.sel.value)
