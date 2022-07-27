a8dbc4dff6355b8f6ecb0c3478eec072cb3bd7e7
D4WN-9 <nononishant@gmail.com>
2022-07-27 17:16:05 +0000

    fixes

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -11,10 +11,8 @@ async def mux_exhaustive_test(dut):
 	for i in range(31):
 		x = np.roll(input_arr, i)
 		for j in range(31):
-			temp = 'dut.inp'+str(j)+'.value='+str(x[j])
-			exec(temp)
-			for k in range(31):
-				dut.sel.value = k
-				await Timer(1, units='ns')
-				dut._log.info(f'i={i:05} j={j:05} model={x[k]:05} DUT={int(dut.out.value):05}')
-				assert dut.out.value == x[k], 'TEST FAIL with sel={k}'.format(k = dut.sel.value)
+			exec('dut.inp'+str(j)+'.value='+str(x[j]))
+			dut.sel.value = j
+			await Timer(1, units='ns')
+			# dut._log.info(f'i={i:05} j={j:05} model={x[j]:05} DUT={int(dut.out.value):05}')
+			assert dut.out.value == x[j], "TEST FAIL"
