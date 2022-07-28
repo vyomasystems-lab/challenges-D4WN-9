d469ec7dd3ca5a2c07e4e0163010c1a601518366
D4WN-9 <nononishant@gmail.com>
2022-07-28 05:16:26 +0000

    final

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -4,6 +4,7 @@ from cocotb.triggers import Timer
 
 @cocotb.test()
 async def mux_exhaustive_test(dut):
+	e_list = []
 	input_arr = np.int32(np.zeros(31))
 	input_arr[0] = 1
 	for i in range(31):
@@ -13,4 +14,13 @@ async def mux_exhaustive_test(dut):
 			dut.sel.value = j
 			await Timer(1, units='ns')
 			dut._log.info(f'i={i:05} j={j:05} model={x[j]:05} DUT={int(dut.out.value):05}')
-			assert dut.out.value == x[j], "TEST FAIL @ sel = {j}, out = {m}".format(j=dut.sel.vlaue, m=dut.out.value)
+			try:
+				assert dut.out.value == x[j], "TEST FAIL @ sel = {J}, in = {V} out = {M}".format(J=dut.sel.value, V=x[j], M=dut.out.value)
+			except AssertionError as e:
+				print(e)
+				e_list.append(e)
+	if len(e_list)==0:
+		print("NO ERRORS FOUND")
+	else:
+		print(e_list)
+		print(str(len(e_list))+' ERRORS FOUND')
