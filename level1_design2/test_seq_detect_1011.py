edf86547dbb08e20371fe9e738f5dfe3fafb5515
D4WN-9 <nononishant@gmail.com>
2022-07-31 16:41:09 +0000

    update

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -14,13 +14,13 @@ async def test_all_bugs(dut):
 	out_seq = '000000010010000000100000000100000010001'
 
 	for i in range(len(inp_seq)):
-		if i < 3:
+		if i < 5:
 			continue
 		dut.inp_bit.value = int(inp_seq[i])
 		await RisingEdge(dut.clk)
-		dut._log.info(f'input={int(inp_seq[i-3: i+1],2):04}')
-		dut._log.info(f'expected_output={int(out_seq[i],2):01}')
-		dut._log.info(f'actual_output={int(str(dut.seq_seen.value),2):05}')
+		dut._log.info(f'input={int(inp_seq[i-3: i+2]):05}')
+		dut._log.info(f'model={int(out_seq[i]):01}')
+		dut._log.info(f'DUT={int(dut.seq_seen.value):01}')
 		try:
 			assert dut.seq_seen.value == out_seq[i], "TEST FAIL @ inp = {A}, out = {B}".format(A=dut.inp_bit.value, B=dut.seq_seen.value)
 		except AssertionError as e:
